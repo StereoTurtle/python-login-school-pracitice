7b045d3ffb71a3315b2fb3f153296e5c223fb289
Adrian Ozuna <87553186+StereoTurtle@users.noreply.github.com>
2022-01-14 14:55:32 -0400

    Added else statements and added missing breaks.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 print("Adrian Ozuna | #19 | 5toC")
 print("Crear un programa que pida al usuario que se registre en un sistema")
 
-accion = 0
 usuario1 = ""
 password1 = ""
 usuario2 = ""
@@ -18,70 +17,79 @@ while True:
         break
     else:
         continue
-    
-
-if(accion == 1):
-    while True:
-        print("Bienvenido al login.")
-        userLogin = input("Ingrese el usuario: ")
-        passwordLogin = input("Ingrese la contraseña: ")
-        
-        if(userLogin == usuario1 and passwordLogin == password1):
-            print("Bienvenido ", usuario1)
-            break
-        elif(userLogin == usuario2 and passwordLogin == password2):
-            print("Bienvenido", usuario2)
-            break
-        else:
-            print("El usuario y/o la contraseña son incorrectos.")
-            continue
-elif(accion == 2):
-    print("Bienvenido al registro.") 
-    while True:
-        userRegister = input("Ingrese el usuario a registrar: ")
-        if((userRegister == usuario1) or (userRegister == usuario2)):
-            print("Este usuario ya existe")
-            continue
-        else:    
-            usuario1 = userRegister
+while True:
+    if(accion == 1):
+        while True:
+            print("Bienvenido al login.")
+            userLogin = input("Ingrese el usuario: ")
+            passwordLogin = input("Ingrese la contraseña: ")
             
-            while True:
-                passwordRegister = input("Ingrese la contraseña: ")
-                passwordConfirm = input("Vuelva a escribir la contraseña: ")
-            
-                if(passwordRegister != passwordConfirm):
-                    print("Las contraseñas no conciden. Vuelva a intenarlo.")
-                    continue
-                else:
-                    password1 = passwordRegister
-                    print("Usario registrado correctamente.")
-                    
-                    while True:
-                        eleccionRegistro = int(input("Desea registrar otro usuario?: Si (1) | No (2): "))
-                        if(eleccionRegistro == 1): 
-                            while True:
-                                userRegister = input("Ingrese el usuario a registrar: ")
-                                if(userRegister == usuario2):
-                                    print("Este usuario ya existe")
-                                    continue
-                                else:    
-                                    usuario2 = userRegister
-                                    while True:
-                                        passwordRegister = input("Ingrese la contraseña: ")
-                                        passwordConfirm = input("Vuelva a escribir la contraseña: ")
-                                    
-                                        if(passwordRegister != passwordConfirm):
-                                            print("Las contraseñas no conciden. Vuelva a intenarlo.")
-                                            continue
-                                        else:
-                                            password2 = passwordRegister
-                                            print("Usario registrado correctamente.")
-                                            break
-                                    break
-                        break
+            if(userLogin == usuario1 and passwordLogin == password1):
+                print("Bienvenido ", usuario1)
                 break
+            elif(userLogin == usuario2 and passwordLogin == password2):
+                print("Bienvenido", usuario2)
+                break
+            else:
+                print("El usuario y/o la contraseña son incorrectos. Por favor intentelo nuevamente.")
+                continue
+    elif(accion == 2):
+        while True:
+            print("Bienvenido al registro.") 
+            userRegister = input("Ingrese el usuario a registrar: ")
+            if((userRegister == usuario1) or (userRegister == usuario2)):
+                print("Este usuario ya existe")
+                continue
+            else:    
+                usuario1 = userRegister
+                
+                while True:
+                    passwordRegister = input("Ingrese la contraseña: ")
+                    passwordConfirm = input("Vuelva a escribir la contraseña: ")
+                
+                    if(passwordRegister != passwordConfirm):
+                        print("Las contraseñas no conciden. Vuelva a intenarlo.")
+                        continue
+                    else:
+                        password1 = passwordRegister
+                        print("Usario registrado correctamente.")
+                        
+                        while True:
+                            eleccionRegistro = int(input("Desea registrar otro usuario?: Si (1) | No (2): "))
+                            if(eleccionRegistro == 1): 
+                                while True:
+                                    userRegister = input("Ingrese el usuario a registrar: ")
+                                    if(userRegister == usuario2):
+                                        print("Este usuario ya existe")
+                                        continue
+                                    else:    
+                                        usuario2 = userRegister
+                                        while True:
+                                            passwordRegister = input("Ingrese la contraseña: ")
+                                            passwordConfirm = input("Vuelva a escribir la contraseña: ")
+                                        
+                                            if(passwordRegister != passwordConfirm):
+                                                print("Las contraseñas no conciden. Vuelva a intenarlo.")
+                                                continue
+                                            else:
+                                                password2 = passwordRegister
+                                                print("Usario registrado correctamente.")
+                                                break
+                            elif(eleccionRegistro == 2):
+                                print("gerg")
+                                break
+                            else:
+                                print("La opcion que eligio es incorrecta. Vuelva a intentarlo.")
+                                continue
+                        break
+                    break
+                break
+            break
         break
-
+    else:
+        print("La opcion que eligio es incorrecta. Vuelva a intentarlo.")
+        continue
+    
 while True:
     print("Bienvenido al login.")
     userLogin = input("Ingrese el usuario: ")
@@ -94,5 +102,5 @@ while True:
         print("Bienvenido", usuario2)
         break
     else:
-        print("El usuario y/o la contraseña son incorrectos.")
+        print("El usuario y/o la contraseña son incorrectos. Por favor intentelo nuevamente.")
         continue
